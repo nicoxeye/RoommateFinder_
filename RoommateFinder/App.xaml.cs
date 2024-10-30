c6ae7c12a52f0c7c53f766599c1da9b6c274e522
nicoxeye <nicoxeye@op.pl>
2024-10-30 08:32:49 +0100

    Dodaj pliki projektów.

---
+++ RoommateFinder/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RoommateFinder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
